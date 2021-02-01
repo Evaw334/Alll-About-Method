9efb523695e96e12747b6bfcf13b5986f8d84d07
Eva Washington <eva.washington@patelhs.org>
2021-02-01 14:10:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+
+public static void main(String[] args) {
+    PrintInfo("Eva", 15);
+}
+public static void printinfo(String name, int age) {
+     System.out.println(name + "is" + age + "years old");
+  }
+}
